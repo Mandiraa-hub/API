1185ff2083641e486dfb386e48306fb0e11c20ca
Mandiraa-hub <mairakhanal2060@gmail.com>
2024-12-24 09:57:16 +0545

    Create Blog.route.js

---
+++ routes/Blog.route.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+const { getAllBlogs, addBlog, updateBlog , getById, deleteBlog, getByUserId} = require('../controllers/Blog.controller');
+
+router.get('/', getAllBlogs)
+router.post('/add', addBlog)
+router.get('/:id', getById)
+router.put('/update/:id', updateBlog)
+router.delete('/:id', deleteBlog)
+router.get('/user/:id', getByUserId)
+
+module.exports = router
